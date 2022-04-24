8e92c5eca4a659db7da75e6ac67bcf992ea56816
Shelley Bassett <m.bassett@y7mail.com>
2022-04-24 11:34:54 +1000

    changed ftp upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -14,8 +14,13 @@ module.exports = ({ env }) => ({
         },
     },
     upload: {
-      config: {
-        provider: 'strapi-provider-upload-sftp'
+      provider: 'strapi-provider-upload-ftp-v2',
+      providerOptions: {
+        host: env('SFTP_UPLOAD_HOST'),
+        user: env('SFTP_UPLOAD_USER'),
+        password: env('SFTP_UPLOAD_PASSWORD'),
+        basePath: env('SFTP_UPLOAD_BASEPATH'),
+        baseUrl: env('SFTP_UPLOAD_BASEURL'),
       },
     },
   });
