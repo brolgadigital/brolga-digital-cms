7fd50747bd9a5ccaf4f5c73778c9567193beba14
Shelley Bassett <m.bassett@y7mail.com>
2022-04-28 08:23:32 +1000

    Changed defult port for 1337 to 8080

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   url: 'https://cms.brolgadigital.com.au',
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
