d6734335c653f500f48ce457493be3170adb7c6d
Shelley Bassett <m.bassett@y7mail.com>
2022-04-28 11:28:41 +1000

    Changed defult port to match database

--- config/database.js
+++ config/database.js
@@ -23,7 +23,7 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
+      port: env.int("DATABASE_PORT", 5428),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
